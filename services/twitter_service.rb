e4c50a4d5a254896951cdc8f0ec8b0d1a21083d7
Alisa Chang <laptite@gmail.com>
2020-04-20 21:52:36 -0700

    Remove Twitter keys

--- services/twitter_service.rb
+++ services/twitter_service.rb
@@ -2,10 +2,10 @@ class TwitterService
 
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "QjI9yjrVCr7rkrAVZd6nx9udS"
-      config.consumer_secret     = "rbFaEoiLtQnSb8R8524gTO3cs69gC0rFB1hdDua8tX617Mu69l"
-      config.access_token        = "109004372-vtNnJ2tW1I1GOMnCdlskoiJ6Ci6nPyLKAiEsK7TV"
-      config.access_token_secret = "QdcjIBloYKTjGvI8kLHhSbUrSq4QKaShPSq9ZS9zrJQYN"
+      config.consumer_key        = ""
+      config.consumer_secret     = ""
+      config.access_token        = ""
+      config.access_token_secret = ""
     end
   end
 
